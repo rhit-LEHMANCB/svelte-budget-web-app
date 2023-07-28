fb1c81e31dc33cd61ac8c1a929383b36128b3839
rhit-LEHMANCB <cblehman22@gmail.com>
2023-07-28 18:05:28 -0400

    implement  basic shell

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
+
+	import { AppBar, AppShell, Drawer, drawerStore, type DrawerSettings } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/Navigation/Navigation.svelte';
+
+	const drawerSettings: DrawerSettings = {
+		width: 'w-[80vw] md:w-[280px]',
+	}
+
+	function drawerOpen(): void {
+		drawerStore.open(drawerSettings);
+	}
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+</script>
+
+<Drawer>
+	<Navigation />
+</Drawer>
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<strong class="text-xl uppercase">Skeleton</strong>
+				</div>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+</AppShell>
