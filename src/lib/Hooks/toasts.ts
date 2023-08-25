1bdef9036fbc6f051741bac3ad018a53c8bac6e8
rhit-LEHMANCB <cblehman22@gmail.com>
2023-08-25 17:00:56 -0400

    actions and profile page update

--- src/lib/Hooks/toasts.ts
+++ src/lib/Hooks/toasts.ts
@@ -1,19 +1,19 @@
-import { toastStore, type ToastSettings } from "@skeletonlabs/skeleton";
+import { toastStore, type ToastSettings } from '@skeletonlabs/skeleton';
 
 export function successToast(message: string) {
-const successToast: ToastSettings = {
-    message: message,
-    // Provide any utility or variant background style:
-    background: 'variant-filled-success'
-};
-toastStore.trigger(successToast);
+	const successToast: ToastSettings = {
+		message: message,
+		// Provide any utility or variant background style:
+		background: 'variant-filled-success'
+	};
+	toastStore.trigger(successToast);
 }
 
 export function errorToast(message: string) {
-const errorToast: ToastSettings = {
-    message: message,
-    // Provide any utility or variant background style:
-    background: 'variant-filled-error'
-};
-toastStore.trigger(errorToast);
+	const errorToast: ToastSettings = {
+		message: message,
+		// Provide any utility or variant background style:
+		background: 'variant-filled-error'
+	};
+	toastStore.trigger(errorToast);
 }
