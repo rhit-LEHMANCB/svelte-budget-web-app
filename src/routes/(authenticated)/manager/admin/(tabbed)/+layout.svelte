dd89e2486e037f1476ddb479d7151bc39ed7183b
BuildTools <unconfigured@null.spigotmc.org>
2023-09-11 23:23:30 -0400

    rework admin tabs

---
+++ src/routes/(authenticated)/manager/admin/(tabbed)/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
+	import type { LayoutData } from './$types';
+	import { IconBuildingEstate, IconUsers } from '@tabler/icons-svelte';
+	import { page } from '$app/stores';
+</script>
+
+<div class="card m-5">
+	<!-- TODO: separate these two tabs into different routes with common layout -->
+	<TabGroup>
+		<TabAnchor href="/manager/admin/properties" selected={$page.url.pathname === '/manager/admin/properties'}>
+			<div class="flex gap-2">
+				<IconBuildingEstate />
+				<span>Properties</span>
+			</div>
+		</TabAnchor>
+		<TabAnchor href="/manager/admin/users" selected={$page.url.pathname === '/manager/admin/users'}
+			><div class="flex gap-2">
+				<IconUsers />
+				<span>Users</span>
+			</div></TabAnchor
+		>
+		<!-- Tab Panels --->
+		<svelte:fragment slot="panel">
+			<slot />
+		</svelte:fragment>
+	</TabGroup>
+</div>
