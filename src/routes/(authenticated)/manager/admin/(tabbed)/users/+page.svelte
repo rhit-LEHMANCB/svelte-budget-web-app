735f124b2c58fa7d6919ae3aa4515836883de9e8
BuildTools <unconfigured@null.spigotmc.org>
2023-09-11 23:27:20 -0400

    fix

--- src/routes/(authenticated)/manager/admin/(tabbed)/users/+page.svelte
+++ src/routes/(authenticated)/manager/admin/(tabbed)/users/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { PageData } from '../../$types';
+	import type { PageData } from '../$types';
 	import UsersTab from '$lib/Components/Admin/UsersTab.svelte';
 
 	export let data: PageData;
