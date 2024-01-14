cb768d1f5c847e50b643a72863b67c15ade8a464
rhit-LEHMANCB <cblehman22@gmail.com>
2024-01-14 16:33:24 -0500

    beginnings of stripe

--- src/routes/(authenticated)/manager/payment/+page.svelte
+++ src/routes/(authenticated)/manager/payment/+page.svelte
@@ -1,4 +1,31 @@
 <script lang="ts">
+	import { goto, invalidateAll } from "$app/navigation";
+	import { successToast, errorToast } from "$lib/Hooks/toasts";
+	import { getToastStore } from "@skeletonlabs/skeleton";
+
+    const toastStore = getToastStore();
+
+    async function startCheckout() {
+        const lookupKey = 'basic';
+        const response = await fetch(`/api/stripe/create-checkout-session`, {
+			method: 'POST',
+            headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ lookupKey })
+		});
+		if (response.ok) {
+            const body = await response.json();
+            goto(body.url);
+			successToast('User Successfully Removed.', toastStore);
+		} else {
+			errorToast('Error removing user.', toastStore);
+		}
+    }
 </script>
 
-<h1>Payment</h1>
+<div>
+    <button class='btn variant-filled-primary' on:click={startCheckout}>
+        Pay
+    </button>
+</div>
